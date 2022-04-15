957e7ec3d817dab1cbd8c3da65e0343b1ade4028
Cengiz Bugra Aksakal <aksakalcengiz@hotmail.com>
2022-04-15 14:58:02 +0300

    ArrayFindMethod lab completed.

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // code your solution here
+function superbowlWin(arr) {
+    const result = arr.find(element => element.result === "W");
+    return result === undefined ? undefined : result.year;
+}
